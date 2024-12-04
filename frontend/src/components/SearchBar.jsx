8d556d068ae61e50ad6ed3c168e8efaeb2c24987
Ashish Bhati <ashbhati26@gmail.com>
2024-12-04 22:32:44 +0530

    responsive search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,28 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useLocation } from "react-router-dom";
 
 const SearchBar = () => {
-    const {search, setSearch, showSearch, setShowSearch} = useContext(ShopContext);
-    const [visible, setVisible] = useState(false)
-    const location = useLocation();
+  const { search, setSearch, showSearch, setShowSearch } =
+    useContext(ShopContext);
+  const [visible, setVisible] = useState(false);
+  const location = useLocation();
 
-    useEffect(()=>{
-        if (location.pathname.includes('collection')) {
-            setVisible(true);
-        }
-        else{
-            setVisible(false)
-        }
-    },[location])
+  useEffect(() => {
+    if (location.pathname.includes("collection")) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }, [location]);
   return showSearch && visible ? (
-    <div className='text-center'>
-        <div className='inline-flex items-center justify-center border-b border-gray-400 px-5 py-2 my-5 mx-3  w-3/4 sm:w-1/2'>
-        <input value={search} onChange={(e)=> setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-[2vw] font-light text-center' type="text" placeholder='Search...' />
-        </div>
+    <div className="text-center">
+      <div className="inline-flex items-center justify-center border-b border-gray-400 px-5 py-2 my-5 mx-3 w-3/4 sm:w-1/2">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="flex-1 outline-none bg-inherit font-light text-center text-[4vw] sm:text-lg md:text-xl"
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
     </div>
-  ) : null
-}
+  ) : null;
+};
 
-export default SearchBar
+export default SearchBar;
