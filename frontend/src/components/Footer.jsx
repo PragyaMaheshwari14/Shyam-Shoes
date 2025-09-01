a26b4ef503b38047d0dc3237822729d5f7ad4a42
Ashish Bhati <ashbhati26@gmail.com>
2025-09-01 23:31:55 +0530

    changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Eyes from './Eyes'
 const Footer = () => {
   return (
     <div className='bg-[#111111] py-4 flex flex-col text-sky-50'>
-        <div className='bg-[#bf111e] h-[20vw] mx-[5vw] rounded-b-2xl items-center justify-between text-[#111111] hidden md:flex'>
+        <div className='bg-blue-300 h-[20vw] mx-[5vw] rounded-2xl items-center justify-between text-[#111111] hidden md:flex'>
             <div className='left-heading w-[30%] h-full flex flex-col justify-center items-center ml-[5vw] p-4'>
               <h1 className='text-[4vw] md:text-[4vw] font-krona tracking-tight'>Let's Shop</h1>
             </div>
